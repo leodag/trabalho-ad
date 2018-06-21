a88e99c017142ded23918704e162eac99a14af0e
Leonardo Dagnino <leodag.sch@gmail.com>
2018-06-21 11:34:15 -0300

    ideias

--- lib/fila_simulada.ex
+++ lib/fila_simulada.ex
@@ -5,14 +5,24 @@ defmodule FilaSimulada do
     Agent.start_link(&PQueue2.new/0)
   end
 
-  def put(queue, pid, priority) do
-    Agent.update(queue, &PQueue2.put(&1, pid, priority))
+  def put(queue, pid, time) do
+    Agent.update(queue, &PQueue2.put(&1, pid, time))
+  end
+
+  def pop_and_replace(queue) do
+    case PQueue2.pop_with_priority(queue) do
+      {{pid, time}, queue} ->
+	send pid, {:get, self()}
+	receive do
+	  {:put, time, pid} -> {:put, time, pid}
+	end
+    end
   end
 
   def get(queue) do
     case Agent.get_and_update(queue, &PQueue2.pop_with_priority(&1)) do
-      {pid, prio} ->
-	{:reply, pid, prio}
+      {pid, time} ->
+	{:reply, time, pid}
       _ ->
 	:empty
     end
