d0da3dfd3c29b8d169d7dcb82ba8744107af94d6
Leonardo Dagnino <leodag.sch@gmail.com>
2018-07-15 22:31:32 -0300

    adicionado teste de sanidade dos geradores de pacotes

--- test/sanity_check_generators.exs
+++ test/sanity_check_generators.exs
@@ -4,7 +4,8 @@ defmodule GeneratorsSanityCheck do
   test "sanity check PacketGenerator (all times are increasing)" do
     PacketGenerator.start_link([], name: PacketGen)
 
-    times = for <- 1..500 do
+    # obtém os tempos dos primeiros 500 pacotes
+    times = for _ <- 1..500 do
       PacketGenerator.get_packet(PacketGen).time
     end
 
@@ -14,7 +15,7 @@ defmodule GeneratorsSanityCheck do
   test "sanity check VoiceGenerator (all times are increasing)" do
     VoiceGenerator.start_link([], name: PacketGen)
 
-    times = for <- 1..500 do
+    times = for _ <- 1..500 do
       PacketGenerator.get_packet(PacketGen).time
     end
 
@@ -24,16 +25,17 @@ defmodule GeneratorsSanityCheck do
   test "sanity check DataGenerator (all times are increasing)" do
     DataGenerator.start_link([], name: PacketGen)
 
-    times = for <- 1..500 do
+    times = for _ <- 1..500 do
       PacketGenerator.get_packet(PacketGen).time
     end
 
     assert are_increasing?(times)
   end
 
+  # Verifica se os números estão em ordem crescente
   defp are_increasing?(numbers) when is_list(numbers) do
     {status, _} =
-      List.foldr(times, {true, 0},
+      List.foldl(numbers, {true, 0},
 	fn(next, {status, last}) ->
 	  {status and last <= next, next}
 	end)
